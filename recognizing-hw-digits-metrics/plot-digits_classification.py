3845b1271412394a258260c1ff08b9ff491af8b1
kalathurkishore <kumarkishorekalathur@gmail.com>
2021-09-10 22:24:29 +0530

    metrics updated

--- recognizing-hw-digits-metrics/plot-digits_classification.py
+++ recognizing-hw-digits-metrics/plot-digits_classification.py
@@ -26,26 +26,71 @@ digits = datasets.load_digits()
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
+
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.3, shuffle=False)
+
+clf = svm.SVC(gamma=0.0000001)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 0.0000001 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=0.000001)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 0.000001 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=0.00001)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 0.00001 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=0.0001)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 0.0001 is ", metrics.accuracy_score(predicted,y_test))
 
 clf = svm.SVC(gamma=0.001)
 clf.fit(X_train, y_train)
 predicted = clf.predict(X_test)
 print("Accuracy for gamma 0.001 is ", metrics.accuracy_score(predicted,y_test))
 
-clf = svm.SVC(gamma=0.002)
+clf = svm.SVC(gamma=0.01)
 clf.fit(X_train, y_train)
 predicted = clf.predict(X_test)
-print("Accuracy for gamma 0.002 is ", metrics.accuracy_score(predicted,y_test))
+print("Accuracy for gamma 0.01 is ", metrics.accuracy_score(predicted,y_test))
 
-clf = svm.SVC(gamma=0.003)
+clf = svm.SVC(gamma=0.1)
 clf.fit(X_train, y_train)
 predicted = clf.predict(X_test)
-print("Accuracy for gamma 0.003 is ", metrics.accuracy_score(predicted,y_test))
+print("Accuracy for gamma 0.1 is ", metrics.accuracy_score(predicted,y_test))
 
-clf = svm.SVC(gamma=0.004)
+clf = svm.SVC(gamma=1)
 clf.fit(X_train, y_train)
 predicted = clf.predict(X_test)
-print("Accuracy for gamma 0.004 is ", metrics.accuracy_score(predicted,y_test))
+print("Accuracy for gamma 1 is ", metrics.accuracy_score(predicted,y_test))
 
+clf = svm.SVC(gamma=10)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 10 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=100)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 100 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=1000)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 1000 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=10000)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 10000 is ", metrics.accuracy_score(predicted,y_test))
+
+clf = svm.SVC(gamma=100000)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("Accuracy for gamma 100000 is ", metrics.accuracy_score(predicted,y_test))
