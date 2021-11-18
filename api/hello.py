df4e0a9881525dc1b7bf0592a7e55c106606bc0c
kalathurkishore <kumarkishorekalathur@gmail.com>
2021-11-19 01:12:33 +0530

    updated model

--- api/hello.py
+++ api/hello.py
@@ -1,16 +1,39 @@
 from flask import Flask
+from flask import request
 from flask_restx import Resource, Api
 from werkzeug.utils import cached_property
+from joblib import dump, load
+import numpy as np
+
+best_model_path = '../models/tt_0.15_val_0.15_rescale_1_gamma_0.001/model.joblib'
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 
+clf = load(best_model_path)
 
-@api.route('/hello')
+'''@api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+'''
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    input_json = request.json
+    image = input_json['image']
+    image = np.array(image).reshape(1,-1)
+    prediction = clf.predict(image)
+    output = '\n Predicted digit is : ' + str(prediction[0]) + '\n\n'
+    return  output
+    #return "<p>image received</p>"
 
 
+
+'''
 if __name__ == '__main__':
-     app.run(host='0.0.0.0')
+     app.run(debug=True)'''
